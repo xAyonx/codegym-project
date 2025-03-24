8d0c9d8b813c482a3143225c04e7d2de79cf0d4a
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-03-24 15:17:07 +0100

    Initial commit

---
+++ src/de/codegym/task/task04/task0420/Solution.java
@@ -0,0 +1,39 @@
+package de.codegym.task.task04.task0420;
+
+/* 
+Drei Zahlen sortieren
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        if (a <= b && a <= c) {
+            if (b <= c) {
+                System.out.println(c + " " + b + " " + a);
+            } else {
+                System.out.println(b + " " + c + " " + a);
+            }
+        } else if (b <= a && b <= c) {
+            if (a <= c) {
+                System.out.println(c + " " + a + " " + b);
+            } else {
+                System.out.println(a + " " + c + " " + b);
+            }
+        } else {
+            if (a <= b) {
+                System.out.println(b + " " + a + " " + c);
+            } else {
+                System.out.println(a + " " + b + " " + c);
+            }
+        }
+    }
+}
+
+
