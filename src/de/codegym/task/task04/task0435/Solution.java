de0df85d0359b461f1ba03dbd6b14444ef138626
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-04-03 09:20:26 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/de/codegym/task/task04/task0435/Solution.java
@@ -0,0 +1,16 @@
+package de.codegym.task.task04.task0435;
+
+/* 
+Gerade Zahlen
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 2; i<=100; i=i+2)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
