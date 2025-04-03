1014b23aab7917c306400ffb4c5a579aa288e707
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-04-03 10:27:12 +0200

    Merge remote-tracking branch 'origin/master'

--- src/de/codegym/task/task04/task0436/Solution.java
+++ src/de/codegym/task/task04/task0436/Solution.java
@@ -1,15 +1,24 @@
 package de.codegym.task.task04.task0436;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-/* 
+/*
 Ein Rechteck zeichnen
 */
 
-import java.io.*;
-
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //schreib hier deinen Code
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bis.readLine());
+        int n = Integer.parseInt(bis.readLine());
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print('8');
+            }
+
+            System.out.println();
+        }
     }
 }
