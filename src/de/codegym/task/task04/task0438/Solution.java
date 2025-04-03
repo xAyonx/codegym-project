6c2646fbc184cd79411d4a878d031e744d21859e
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-04-03 11:08:57 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/de/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,18 @@
+package de.codegym.task.task04.task0438;
+
+/* 
+Linien zeichnen
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        for (int zeile = 0; zeile < 10; zeile++) {
+            System.out.println("8");
+        }
+
+        for (int spalt = 0; spalt < 10; spalt++) {
+            System.out.print("8");
+        }
+    }
+}
