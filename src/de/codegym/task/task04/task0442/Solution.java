2103c43eb4245e58e6bf9505708d2a112ac816db
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-04-09 10:18:00 +0200

    Merge remote-tracking branch 'origin/master'

--- src/de/codegym/task/task04/task0442/Solution.java
+++ src/de/codegym/task/task04/task0442/Solution.java
@@ -5,10 +5,24 @@ package de.codegym.task.task04.task0442;
 Addieren
 */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //schreib hier deinen Code
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+
+        while (true){
+            int a = Integer.parseInt(bis.readLine());
+            sum+=a;
+            if (a==-1){
+                break;
+            }
+        }
+        System.out.println(sum);
+
+
+
     }
 }
