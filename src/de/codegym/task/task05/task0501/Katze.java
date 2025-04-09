2eb49a488be3d14a78deab3ee35a9ea8cae83533
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-04-09 15:02:55 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/de/codegym/task/task05/task0501/Katze.java
@@ -0,0 +1,16 @@
+package de.codegym.task.task05.task0501;
+
+/* 
+Eine Katze erstellen
+*/
+
+public class Katze {
+    public String name;
+    public int alter;
+    private int gewicht;
+    protected int staerke;
+
+    public static void main(String[] args) {
+
+    }
+}
