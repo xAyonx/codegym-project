b55ceab09fdec0f8d669d998ac5363f20182dd02
Ayon <115979462+xAyonx@users.noreply.github.com>
2025-07-04 09:35:07 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/de/codegym/task/task05/task0506/Person.java
@@ -0,0 +1,18 @@
+package de.codegym.task.task05.task0506;
+
+/* 
+Menschen
+*/
+
+public class Person {
+    String name;
+    int alter;
+    String adresse;
+    char geschlecht;
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
